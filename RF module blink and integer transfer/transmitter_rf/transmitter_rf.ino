f4e78eff068b7bf68ed6c21ae7af72b56a6575b1
ganeshamulay <ganeshamulay@gmail.com>
2015-06-09 16:45:44 +0530

    transmit blinking on rf module

---
+++ RF module blink and integer transfer/transmitter_rf/transmitter_rf.ino
@@ -0,0 +1,27 @@
+#include <VirtualWire.h>
+
+//simple Tx on pin D12
+//Written By : Mohannad Rawashdeh
+// 3:00pm , 13/6/2013
+//http://www.genotronex.com/
+//..................................
+//#include <VirtualWire.h>
+char *controller;
+void setup() {
+  pinMode(8,OUTPUT);
+vw_set_ptt_inverted(true); //
+vw_set_tx_pin(12);
+vw_setup(2000);// speed of data transfer Kbps
+}
+void loop(){
+controller="1"  ;
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(8,1);
+delay(2000);
+controller="0"  ;
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(8,0);
+delay(2000);
+}
